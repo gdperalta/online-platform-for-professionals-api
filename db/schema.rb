be1c2f3096c9df03cf3c5aa0d9072c4e9efcd56c
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-07 12:39:03 +0800

    Add clients and professionals to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_153104) do
+ActiveRecord::Schema.define(version: 2022_05_07_022021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "expired_at", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "professionals", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "field"
+    t.string "license_number"
+    t.string "office_address"
+    t.text "headline"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_professionals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +62,5 @@ ActiveRecord::Schema.define(version: 2022_05_06_153104) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
 end
