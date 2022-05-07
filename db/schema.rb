5f8eb0853b64e135630881c5eae5c5e8e6ac7c74
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-07 23:32:26 +0800

    Create table for reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_022021) do
+ActiveRecord::Schema.define(version: 2022_05_07_140037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2022_05_07_022021) do
     t.index ["user_id"], name: "index_professionals_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "professional_id", null: false
+    t.bigint "client_id", null: false
+    t.integer "rating"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_id"], name: "index_reviews_on_client_id"
+    t.index ["professional_id"], name: "index_reviews_on_professional_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +74,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_022021) do
   end
 
   add_foreign_key "clients", "users"
+  add_foreign_key "reviews", "clients"
+  add_foreign_key "reviews", "professionals"
 end
