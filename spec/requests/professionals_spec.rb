444ca9f2c340f85b9b44fbc0a8c73b7075a21c5d
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-07 23:34:19 +0800

    Add tests for professional

--- spec/requests/professionals_spec.rb
+++ spec/requests/professionals_spec.rb
@@ -1,22 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe '/professionals', type: :request do
-  # This should return the minimal set of attributes required to create a valid
-  # Professional. As you add validations to Professional, be sure to
-  # adjust the attributes here as well.
-  # let(:valid_attributes) do
-  #   skip('Add a hash of attributes valid for your model')
-  # end
+  let(:valid_attributes) do
+    attributes_for(:professional)
+  end
 
   # let(:invalid_attributes) do
   #   skip('Add a hash of attributes invalid for your model')
   # end
 
-  # This should return the minimal set of values that should be in the headers
-  # in order to pass any filters (e.g. authentication) defined in
-  # ProfessionalsController, or in your router and rack
-  # middleware. Be sure to keep this updated too.
-  let!(:professional) { create(:user, :professional) }
+  let!(:professional) { create(:professional) }
   let(:valid_headers) do
     post '/login', params: { user: { email: 'pro@email.com', password: 'password' } }
     { 'Accept': 'application/json', 'Authorization': response.headers['Authorization'] }
@@ -25,19 +18,29 @@ RSpec.describe '/professionals', type: :request do
   describe 'GET /index' do
     it 'renders a successful response' do
       get professionals_path, headers: valid_headers
+      data = response.parsed_body['data']
+
       expect(response).to be_successful
-      field = response.parsed_body['data'].first['attributes']['field']
-      expect(field).to eq('Electrical Engineer')
+      expect(data).to_not be_nil
+      expect(data).to_not be_empty
     end
   end
 
-  # describe 'GET /show' do
-  #   it 'renders a successful response' do
-  #     professional = Professional.create! valid_attributes
-  #     get professional_url(professional), as: :json
-  #     expect(response).to be_successful
-  #   end
-  # end
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      get professional_path(professional), headers: valid_headers, as: :json
+      data = response.parsed_body['data']
+      attributes = response.parsed_body['data']['attributes']
+
+      expect(response).to be_successful
+      expect(data).to include('id')
+      expect(data).to include('attributes')
+      expect(attributes).to include('field' => 'Electrical Engineer',
+                                    'licenseNumber' => '0012345',
+                                    'headline' => 'MyHeadline',
+                                    'officeAddress' => 'Office Address')
+    end
+  end
 
   # describe 'POST /create' do
   #   context 'with valid parameters' do
@@ -54,7 +57,7 @@ RSpec.describe '/professionals', type: :request do
   #       expect(response).to have_http_status(:created)
   #       expect(response.content_type).to match(a_string_including('application/json'))
   #     end
-  #   end
+  # end
 
   #   context 'with invalid parameters' do
   #     it 'does not create a new Professional' do
@@ -73,46 +76,56 @@ RSpec.describe '/professionals', type: :request do
   #   end
   # end
 
-  # describe 'PATCH /update' do
-  #   context 'with valid parameters' do
-  #     let(:new_attributes) do
-  #       skip('Add a hash of attributes valid for your model')
-  #     end
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { field: 'Architect', license_number: '0054321', office_address: 'Quezon City', headline: 'MyText' }
+      end
 
-  #     it 'updates the requested professional' do
-  #       professional = Professional.create! valid_attributes
-  #       patch professional_url(professional),
-  #             params: { professional: new_attributes }, headers: valid_headers, as: :json
-  #       professional.reload
-  #       skip('Add assertions for updated state')
-  #     end
+      it 'updates the requested professional' do
+        patch professional_path(professional),
+              params: { professional: new_attributes }, headers: valid_headers, as: :json
+        professional.reload
+        expect(professional.field).to eql('Architect')
+        expect(professional.license_number).to eql('0054321')
+        expect(professional.office_address).to eql('Quezon City')
+        expect(professional.headline).to eql('MyText')
+      end
 
-  #     it 'renders a JSON response with the professional' do
-  #       professional = Professional.create! valid_attributes
-  #       patch professional_url(professional),
-  #             params: { professional: new_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:ok)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
+      it 'renders a JSON response with the professional' do
+        patch professional_url(professional),
+              params: { professional: new_attributes }, headers: valid_headers, as: :json
 
-  #   context 'with invalid parameters' do
-  #     it 'renders a JSON response with errors for the professional' do
-  #       professional = Professional.create! valid_attributes
-  #       patch professional_url(professional),
-  #             params: { professional: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-  # end
+        data = response.parsed_body['data']
+        attributes = response.parsed_body['data']['attributes']
 
-  # describe 'DELETE /destroy' do
-  #   it 'destroys the requested professional' do
-  #     professional = Professional.create! valid_attributes
-  #     expect do
-  #       delete professional_url(professional), headers: valid_headers, as: :json
-  #     end.to change(Professional, :count).by(-1)
-  #   end
-  # end
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to match(a_string_including('application/json'))
+        expect(data).to include('id')
+        expect(data).to include('attributes')
+        expect(attributes).to include('field' => 'Architect',
+                                      'licenseNumber' => '0054321',
+                                      'headline' => 'MyText',
+                                      'officeAddress' => 'Quezon City')
+      end
+    end
+
+    # context 'with invalid parameters' do
+    #   it 'renders a JSON response with errors for the professional' do
+    #     professional = Professional.create! valid_attributes
+    #     patch professional_url(professional),
+    #           params: { professional: invalid_attributes }, headers: valid_headers, as: :json
+    #     expect(response).to have_http_status(:unprocessable_entity)
+    #     expect(response.content_type).to match(a_string_including('application/json'))
+    #   end
+    # end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'destroys the requested professional' do
+      expect do
+        delete professional_url(professional), headers: valid_headers, as: :json
+      end.to change(Professional, :count).by(-1)
+    end
+  end
 end
