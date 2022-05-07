7a59abcd3a2706e012e699338ca862084ca95860
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-07 12:39:17 +0800

    Create factory bot for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :user do
-    
+    password { 'password' }
+    first_name { 'John' }
+    last_name { 'Cruz' }
+    contact_number { '9151234567' }
+    city { 'Manila' }
+    region { 'NCR' }
+
+    trait :professional do
+      id { 1 }
+      email { 'pro@email.com' }
+      role { 'professional' }
+
+      before(:create) do |user|
+        user.build_professional(field: 'Electrical Engineer',
+                                license_number: '0012345')
+      end
+    end
   end
 end
