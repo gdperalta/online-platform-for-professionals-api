0ce1b08548533aca825c1f43b08fbd786a98c1f5
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 22:08:38 +0800

    Add relationships

--- app/serializers/professional_serializer.rb
+++ app/serializers/professional_serializer.rb
@@ -2,4 +2,8 @@ class ProfessionalSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
   attributes :field, :license_number, :office_address, :headline
+  belongs_to :user
+  has_many :work_portfolios
+  has_many :services
+  has_many :reviews
 end
