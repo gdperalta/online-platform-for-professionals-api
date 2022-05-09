109673221616ad90cf87b9b765b017d7c688a8f6
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 15:44:31 +0800

    Generate scaffold for work portfolio Edit rendered json to become serialized Edit work_portfolio to be associated with professional

---
+++ app/controllers/work_portfolios_controller.rb
@@ -0,0 +1,51 @@
+class WorkPortfoliosController < ApplicationController
+  before_action :set_professional, only: %i[index create]
+  before_action :set_work_portfolio, only: %i[show update destroy]
+
+  def index
+    @work_portfolios = @professional.work_portfolios
+
+    render json: WorkPortfolioSerializer.new(@work_portfolios)
+  end
+
+  def show
+    render json: WorkPortfolioSerializer.new(@work_portfolio)
+  end
+
+  def create
+    @work_portfolio = @professional.work_portfolios.build(work_portfolio_params)
+
+    if @work_portfolio.save
+      render json: WorkPortfolioSerializer.new(@work_portfolio), status: :created
+    else
+      render json: WorkPortfolioSerializer.new(@work_portfolio.errors), status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @work_portfolio.update(work_portfolio_params)
+      render json: WorkPortfolioSerializer.new(@work_portfolio)
+    else
+      render json: WorkPortfolioSerializer.new(@work_portfolio.errors), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @work_portfolio.destroy
+  end
+
+  private
+
+  def set_professional
+    @professional = Professional.find(params[:professional_id])
+  end
+
+  def set_work_portfolio
+    @professional = set_professional
+    @work_portfolio = WorkPortfolio.find(params[:id])
+  end
+
+  def work_portfolio_params
+    params.require(:work_portfolio).permit(:professional_id, :title, :details)
+  end
+end
