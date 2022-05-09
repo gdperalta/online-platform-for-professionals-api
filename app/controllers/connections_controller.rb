c8d559ff753b6d45dda0a4795770fa139be9d05a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 14:26:37 +0800

    Add set_professional method Used professional association for setting and getting connection data

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,21 +1,20 @@
 class ConnectionsController < ApplicationController
-  before_action :set_connection, only: %i[show update destroy]
+  before_action :set_professional, only: %i[index create]
+  before_action :set_connection, only: %i[show destroy]
 
-  # GET /connections
   def index
-    @connections = Connection.all
+    @connections = @professional.connections
 
     render json: ConnectionSerializer.new(@connections)
   end
 
-  # GET /connections/1
   def show
     render json: ConnectionSerializer(@connection)
   end
 
-  # POST /connections
+  # TODO: create classification based on user role
   def create
-    @connection = Connection.new(connection_params)
+    @connection = @professional.connections.build(connection_params)
 
     if @connection.save
       render json: ConnectionSerializer(@connection), status: :created, location: @connection
@@ -33,19 +32,20 @@ class ConnectionsController < ApplicationController
   #   end
   # end
 
-  # DELETE /connections/1
   def destroy
     @connection.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_connection
     @connection = Connection.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+  def set_professional
+    @professional = Professional.find(params[:professional_id])
+  end
+
   def connection_params
     params.require(:connection).permit(:professional_id, :client_id)
   end
