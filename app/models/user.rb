7f404ad84bf3a1b1149a8e46ce98f94b9d58bd80
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 23:33:58 +0800

    Add confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   has_one :professional, dependent: :destroy
   has_one :client, dependent: :destroy
