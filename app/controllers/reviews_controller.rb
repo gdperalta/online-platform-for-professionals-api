b367cd2fc0ff766c24f4599081bf397923aa9be4
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 14:29:43 +0800

    Used professional association for getting reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,27 @@
 class ReviewsController < ApplicationController
-  before_action :set_professional, only: %i[index show update destroy]
+  before_action :set_professional, only: %i[index create]
   before_action :set_review, only: %i[show update destroy]
 
-  # GET /reviews
   def index
     @reviews = @professional.reviews
 
     render json: ReviewSerializer.new(@reviews)
   end
 
-  # GET /reviews/1
   def show
     render json: ReviewSerializer.new(@review)
   end
 
-  # POST /reviews
   def create
-    @review = Review.new(review_params)
+    @review = @professional.reviews.build(review_params)
 
     if @review.save
-      render json: ReviewSerializer.new(@review), status: :created, location: @review
+      render json: ReviewSerializer.new(@review), status: :created
     else
       render json: ReviewSerializer.new(@review.errors), status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /reviews/1
   def update
     if @review.update(review_params)
       render json: ReviewSerializer.new(@review)
@@ -34,23 +30,21 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
   def destroy
     @review.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_review
-    @review = Review.find(params[:id])
+    @professional = set_professional
+    @review = @professional.reviews.find(params[:id])
   end
 
   def set_professional
     @professional = Professional.find(params[:professional_id])
   end
 
-  # Only allow a list of trusted parameters through.
   def review_params
     params.require(:review).permit(:professional_id, :client_id, :rating, :body)
   end
