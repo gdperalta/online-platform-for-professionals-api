5d712bc003065623c4a0ae840ae27230e97d3dd7
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 14:28:21 +0800

    Add classification field in table Connection

--- app/serializers/connection_serializer.rb
+++ app/serializers/connection_serializer.rb
@@ -1,4 +1,5 @@
 class ConnectionSerializer
   include JSONAPI::Serializer
-  attributes :professional_id, :client_id
+  set_key_transform :camel_lower
+  attributes :professional_id, :client_id, :classification
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_09_031508) do
+ActiveRecord::Schema.define(version: 2022_05_09_062210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_05_09_031508) do
     t.bigint "client_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "classification", null: false
     t.index ["client_id"], name: "index_connections_on_client_id"
     t.index ["professional_id"], name: "index_connections_on_professional_id"
   end
