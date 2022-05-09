c2219ca2d2c9e1ead9071fb002d16c9600009311
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-09 15:47:05 +0800

    Add relationship with services and work_portfolios

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -2,5 +2,7 @@ class Professional < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :connections, dependent: :destroy
+  has_many :services, dependent: :destroy
+  has_many :work_portfolios, dependent: :destroy
   has_many :clients, through: :connections
 end
