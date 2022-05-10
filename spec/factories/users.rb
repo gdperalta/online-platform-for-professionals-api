f80c7da99e505a3f45f7d333e94443b6306b6614
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-10 22:18:09 +0800

    Moved contact number to each trait

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,19 +3,20 @@ FactoryBot.define do
     password { 'password' }
     first_name { 'John' }
     last_name { 'Cruz' }
-    contact_number { '9151234567' }
     city { 'Manila' }
     region { 'NCR' }
     confirmed_at { Time.now }
 
     trait :professional do
       id { 1 }
+      contact_number { '9151234567' }
       email { 'pro@email.com' }
       role { 'professional' }
     end
 
     trait :client do
       id { 2 }
+      contact_number { '9151239876' }
       email { 'client@email.com' }
       role { 'client' }
 
@@ -28,6 +29,7 @@ FactoryBot.define do
 
     trait :no_association do
       id { 3 }
+      contact_number { '9158765432' }
       email { 'test@email.com' }
       role { 'professional' }
     end
