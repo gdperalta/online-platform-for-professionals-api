6af86d1abea9feb78f39cfbb5ec88e798559c359
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-10 23:23:03 +0800

    Add validation and tests for service and work_portfolio

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :professional
+  validates :title, presence: true
+  validates :details, presence: true
+  validates :min_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_price, numericality: { greater_than: :min_price }
 end
