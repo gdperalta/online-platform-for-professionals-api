d330227b31155f9cd4f2dea2f539df8d7555af3d
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-10 22:54:06 +0800

    Add validations and test for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :professional
   belongs_to :client
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
 end
