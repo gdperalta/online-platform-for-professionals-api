3fd7a971227687f5806574ea2474d13e3f120388
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-12 10:33:54 +0800

    Add table calendly_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_154750) do
+ActiveRecord::Schema.define(version: 2022_05_11_124839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendly_tokens", force: :cascade do |t|
+    t.bigint "professional_id", null: false
+    t.string "authorization", null: false
+    t.string "user", null: false
+    t.string "organization", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["professional_id"], name: "index_calendly_tokens_on_professional_id"
+  end
 
   create_table "clients", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -103,6 +113,7 @@ ActiveRecord::Schema.define(version: 2022_05_10_154750) do
     t.index ["professional_id"], name: "index_work_portfolios_on_professional_id"
   end
 
+  add_foreign_key "calendly_tokens", "professionals"
   add_foreign_key "clients", "users"
   add_foreign_key "connections", "clients"
   add_foreign_key "connections", "professionals"
