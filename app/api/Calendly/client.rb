23ef299023c8a027f84d033e5c41d4b185b31f0c
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-12 10:30:55 +0800

    Add api wrapper for calendly api

---
+++ app/api/Calendly/client.rb
@@ -0,0 +1,32 @@
+module Calendly
+  class Client
+    attr_reader :organization_uri, :user_uri
+
+    def initialize(response)
+      @response = response
+      @organization_uri = response[:data]['resource']['current_organization']
+      @user_uri = response[:data]['resource']['uri']
+    end
+
+    def self.user(authorization)
+      response = Request.call(http_method: 'get',
+                              endpoint: '/users/me',
+                              authorization: authorization)
+      new(response)
+    end
+
+    def self.events(authorization, params: {})
+      Request.call(http_method: 'get',
+                   endpoint: '/scheduled_events',
+                   authorization: authorization,
+                   params: params)
+    end
+
+    def self.event_invitee(event_id)
+      response = Request.call(http_method: 'get',
+                              endpoint: "/scheduled_events/#{event_id}/invitees",
+                              authorization: authorization)
+      response[:data]['collection'].first['email']
+    end
+  end
+end
