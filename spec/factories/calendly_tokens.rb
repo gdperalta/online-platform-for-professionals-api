a7bafe13993fe8dd1fb79e27bff2d3f03cbd1218
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-13 20:37:54 +0800

    Comment out professional_id

--- spec/factories/calendly_tokens.rb
+++ spec/factories/calendly_tokens.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :calendly_token do
-    professional_id { 1 }
+    # professional_id { 1 }
     authorization { 'authorization-token' }
     # user { 'user_uri' }
     # organization { 'org_uri' }
