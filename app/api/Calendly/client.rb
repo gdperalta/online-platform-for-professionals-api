bcfea9ae31bc325a9c8473d9b7b100d507f304eb
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-16 18:33:35 +0800

    Add create_invitee_no_show and delete_invitee_no_show comment cancel_event in case of need of use

--- app/api/Calendly/client.rb
+++ app/api/Calendly/client.rb
@@ -22,11 +22,31 @@ module Calendly
                    params: params)
     end
 
-    def self.event_invitee(authorization, event_id)
+    def self.event_invitee(authorization, event_uuid)
       response = Request.call(http_method: 'get',
-                              endpoint: "/scheduled_events/#{event_id}/invitees",
+                              endpoint: "/scheduled_events/#{event_uuid}/invitees",
                               authorization: authorization)
-      response[:data]['collection'].first['uri']
+      response[:data]['collection'].first
+    end
+
+    # def self.cancel_event(authorization, event_uuid, cancellation_reason)
+    #   Request.call(http_method: 'post',
+    #                endpoint: "/scheduled_events/#{event_uuid}/cancellation",
+    #                authorization: authorization,
+    #                payload: { "reason": cancellation_reason }.to_json)
+    # end
+
+    def self.create_invitee_no_show(authorization, invitee_link)
+      Request.call(http_method: 'post',
+                   endpoint: '/invitee_no_shows',
+                   authorization: authorization,
+                   payload: { "invitee": invitee_link }.to_json)
+    end
+
+    def self.delete_invitee_no_show(authorization, no_show_link)
+      Request.call(http_method: 'delete',
+                   endpoint: "/invitee_no_shows/#{no_show_link}",
+                   authorization: authorization)
     end
   end
 end
