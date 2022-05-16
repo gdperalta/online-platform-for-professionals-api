2a217dffaabd638700c9ad64b392e242fae6c1b1
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-16 18:41:07 +0800

    Customized connections relationship based on classification

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,10 @@
 class Client < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
-  has_many :connections, dependent: :destroy
-  has_many :professionals, through: :connections
+  has_many :bookings, dependent: :destroy
+
+  has_many :subscriptions, -> { where classification: 'subscription' }, class_name: 'Connection', dependent: :destroy
+  has_many :subscribed_to, through: :subscriptions, source: :professional
+  has_many :professional_list, -> { where classification: 'client_list' }, class_name: 'Connection', dependent: :destroy
+  has_many :professionals, through: :professional_list
 end
