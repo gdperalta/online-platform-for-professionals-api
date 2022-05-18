67d5a9a1c130c7331ed206bdf0572082ce1c6d46
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-18 16:49:34 +0800

    Add authorization for requests using pundit Changed create method of CalendlyToken. Use new method instead of build Removed set_professional

--- app/controllers/calendly_tokens_controller.rb
+++ app/controllers/calendly_tokens_controller.rb
@@ -1,5 +1,4 @@
 class CalendlyTokensController < ApplicationController
-  before_action :set_professional, only: %i[create]
   before_action :set_calendly_token, only: %i[show update destroy]
 
   # def index
@@ -9,21 +8,28 @@ class CalendlyTokensController < ApplicationController
   # end
 
   def show
+    authorize @calendly_token
+
     render json: CalendlyTokenSerializer.new(@calendly_token)
   end
 
   def create
-    @calendly_token = @professional.build_calendly_token(calendly_token_params)
+    @calendly_token = CalendlyToken.new(calendly_token_params)
+    @calendly_token.professional_id = params[:professional_id]
+
+    authorize @calendly_token
 
     if @calendly_token.save
       render json: CalendlyTokenSerializer.new(@calendly_token), status: :created,
-             location: professional_calendly_token_path(@professional, @calendly_token)
+             location: professional_calendly_token_path(@calendly_token.professional, @calendly_token)
     else
       render json: ErrorSerializer.serialize(@calendly_token.errors), status: :unprocessable_entity
     end
   end
 
   def update
+    authorize @calendly_token
+
     if @calendly_token.update(calendly_token_params)
       render json: CalendlyTokenSerializer.new(@calendly_token)
     else
@@ -32,18 +38,15 @@ class CalendlyTokensController < ApplicationController
   end
 
   def destroy
+    authorize @calendly_token
+
     @calendly_token.destroy
   end
 
   private
 
-  def set_professional
-    @professional = Professional.find(params[:professional_id])
-  end
-
   def set_calendly_token
-    @professional = set_professional
-    @calendly_token = @professional.calendly_token
+    @calendly_token = CalendlyToken.find(params[:id])
   end
 
   def calendly_token_params
