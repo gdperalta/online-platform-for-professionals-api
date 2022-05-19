61f68c1b0bf6030ffd4e56fc72a51127c4663561
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-19 16:41:58 +0800

    Add tests for booking requests

--- spec/requests/bookings_spec.rb
+++ spec/requests/bookings_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe '/bookings', type: :request do
   let!(:professional) { create(:professional, :with_calendly_token) }
-  let!(:client) { create(:user, :client) }
+  let!(:client) { create(:user, :client).client }
+  before { Connection.create(professional_id: professional.id, client_id: client.id, classification: 'subscription') }
 
   let(:valid_attributes) do
-    skip('Add a hash of attributes valid for your model')
+    attributes_for(:booking, client_id: client.id)
   end
 
   # let(:invalid_attributes) {
@@ -17,101 +18,170 @@ RSpec.describe '/bookings', type: :request do
     { 'Accept': 'application/json', 'Authorization': response.headers['Authorization'] }
   end
 
-  # describe 'GET /index' do
-  #   it 'renders a successful response' do
-  #     get professional_bookings_url(professional, status: 'active'), headers: valid_headers, as: :json
-  #     p response.parsed_body
-  # expect(response).to be_successful
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      allow(Time).to receive(:now).and_return Time.new(2022, 5, 19)
+      get bookings_url(status: 'active'), headers: valid_headers, as: :json
 
-  # response = Calendly::Client.events(professional.calendly_token.authorization, params: { user: professional.calendly_token.user,
-  #                                                                                         invitee_email: professional.subscribers.first.user.email,
-  #                                                                                         count: 5,
-  #                                                                                         min_start_time: '2020-04-24T01:00:00.000000Z' })
-  #   end
-  # end
+      event = response.parsed_body
+      expect(response).to be_successful
+      expect(event).to_not be_empty
+      expect(event.first).to include('links', 'data')
 
-  # describe 'GET /show' do
-  #   it 'renders a successful response' do
-  #     booking = Booking.create! valid_attributes
-  #     get booking_url(booking), as: :json
-  #     expect(response).to be_successful
-  #   end
-  # end
+      data = event.first['data'].first
+      expect(data).to include('id' => 'ZZZZZZZZZZ')
+      expect(data).to include('type' => 'active_events')
+      expect(data).to include('attributes')
 
-  # describe 'POST /create' do
-  #   context 'with valid parameters' do
-  #     it 'creates a new Booking' do
-  #       expect do
-  #         post bookings_url,
-  #              params: { booking: valid_attributes }, headers: valid_headers, as: :json
-  #       end.to change(Booking, :count).by(1)
-  #     end
+      attributes = data['attributes']
+      expect(attributes).to include('uri' => 'https://api.calendly.com/scheduled_events/ZZZZZZZZZZ',
+                                    'name' => '30 Minute Meeting',
+                                    'status' => 'active')
+    end
+  end
 
-  #     it 'renders a JSON response with the new booking' do
-  #       post bookings_url,
-  #            params: { booking: valid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:created)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
+  describe 'GET /show' do
+    let(:booking) { create(:booking, professional_id: professional.id, client_id: client.id) }
 
-  #   context 'with invalid parameters' do
-  #     it 'does not create a new Booking' do
-  #       expect do
-  #         post bookings_url,
-  #              params: { booking: invalid_attributes }, as: :json
-  #       end.to change(Booking, :count).by(0)
-  #     end
+    it 'renders a successful response' do
+      get booking_url(booking), headers: valid_headers, as: :json
+      data = response.parsed_body['data']
+      attributes = response.parsed_body['data']['attributes']
+      relationships = response.parsed_body['data']['relationships']
 
-  #     it 'renders a JSON response with errors for the new booking' do
-  #       post bookings_url,
-  #            params: { booking: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-  # end
+      expect(response).to be_successful
+      expect(data).to include('id')
+      expect(data).to include('attributes')
+      expect(attributes).to include('clientShowedUp' => true,
+                                    'eventUuid' => 'MyString',
+                                    'inviteeLink' => 'Invitee_link',
+                                    'finished' => true)
+      expect(relationships).to include('professional',
+                                       'client')
+    end
+  end
 
-  # describe 'PATCH /update' do
-  #   context 'with valid parameters' do
-  #     let(:new_attributes) do
-  #       skip('Add a hash of attributes valid for your model')
-  #     end
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Booking' do
+        expect do
+          post professional_bookings_url(professional, invitee_email: 'client@email.com'),
+               params: { booking: valid_attributes }, headers: valid_headers, as: :json
+        end.to change(Booking, :count).by(1)
+      end
 
-  #     it 'updates the requested booking' do
-  #       booking = Booking.create! valid_attributes
-  #       patch booking_url(booking),
-  #             params: { booking: new_attributes }, headers: valid_headers, as: :json
-  #       booking.reload
-  #       skip('Add assertions for updated state')
-  #     end
+      it 'renders a JSON response with the new booking' do
+        post professional_bookings_url(professional, invitee_email: 'client@email.com'),
+             params: { booking: valid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including('application/json'))
+        data = response.parsed_body['data']
+        attributes = response.parsed_body['data']['attributes']
+        relationships = response.parsed_body['data']['relationships']
 
-  #     it 'renders a JSON response with the booking' do
-  #       booking = Booking.create! valid_attributes
-  #       patch booking_url(booking),
-  #             params: { booking: new_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:ok)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
+        expect(data).to include('id')
+        expect(data).to include('attributes')
+        expect(attributes).to include('clientShowedUp' => true,
+                                      'eventUuid' => 'MyString',
+                                      'inviteeLink' => 'Invitee_link',
+                                      'finished' => true)
+        expect(relationships).to include('professional',
+                                         'client')
+      end
 
-  #   context 'with invalid parameters' do
-  #     it 'renders a JSON response with errors for the booking' do
-  #       booking = Booking.create! valid_attributes
-  #       patch booking_url(booking),
-  #             params: { booking: invalid_attributes }, headers: valid_headers, as: :json
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #       expect(response.content_type).to match(a_string_including('application/json'))
-  #     end
-  #   end
-  # end
+      it 'renders a JSON response with the new booking' do
+        valid_attributes[:client_showed_up] = false
+        post professional_bookings_url(professional, invitee_email: 'client@email.com'),
+             params: { booking: valid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including('application/json'))
+        data = response.parsed_body['data']
+        attributes = response.parsed_body['data']['attributes']
+        relationships = response.parsed_body['data']['relationships']
 
-  # describe 'DELETE /destroy' do
-  #   it 'destroys the requested booking' do
-  #     booking = Booking.create! valid_attributes
-  #     expect do
-  #       delete booking_url(booking), headers: valid_headers, as: :json
-  #     end.to change(Booking, :count).by(-1)
-  #   end
-  # end
+        expect(data).to include('id')
+        expect(data).to include('attributes')
+        expect(attributes).to include('clientShowedUp' => false,
+                                      'eventUuid' => 'MyString',
+                                      'inviteeLink' => 'Invitee_link',
+                                      'finished' => true,
+                                      'noShowLink' => 'no_show_link')
+        expect(relationships).to include('professional',
+                                         'client')
+      end
+    end
+
+    # context 'with invalid parameters' do
+    #   it 'does not create a new Booking' do
+    #     expect do
+    #       post bookings_url,
+    #            params: { booking: invalid_attributes }, as: :json
+    #     end.to change(Booking, :count).by(0)
+    #   end
+
+    #   it 'renders a JSON response with errors for the new booking' do
+    #     post bookings_url,
+    #          params: { booking: invalid_attributes }, headers: valid_headers, as: :json
+    #     expect(response).to have_http_status(:unprocessable_entity)
+    #     expect(response.content_type).to match(a_string_including('application/json'))
+    #   end
+    # end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:no_show_booking) do
+        create(:booking, professional_id: professional.id,
+                         client_id: client.id, client_showed_up: false,
+                         no_show_link: 'no_show_link')
+      end
+
+      let(:new_attributes) do
+        { client_showed_up: true }
+      end
+
+      it 'updates the requested booking' do
+        patch booking_url(no_show_booking),
+              params: { booking: new_attributes }, headers: valid_headers, as: :json
+        no_show_booking.reload
+        expect(no_show_booking.client_showed_up).to eql(true)
+        expect(no_show_booking.no_show_link).to be_nil
+      end
+
+      it 'renders a JSON response with the booking' do
+        patch booking_url(no_show_booking),
+              params: { booking: new_attributes }, headers: valid_headers, as: :json
+
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to match(a_string_including('application/json'))
+
+        data = response.parsed_body['data']
+        attributes = response.parsed_body['data']['attributes']
+
+        expect(data).to include('id')
+        expect(data).to include('attributes')
+        expect(attributes).to include('clientShowedUp' => true,
+                                      'noShowLink' => nil)
+      end
+    end
+
+    # context 'with invalid parameters' do
+    #   it 'renders a JSON response with errors for the booking' do
+    #     booking = Booking.create! valid_attributes
+    #     patch booking_url(booking),
+    #           params: { booking: invalid_attributes }, headers: valid_headers, as: :json
+    #     expect(response).to have_http_status(:unprocessable_entity)
+    #     expect(response.content_type).to match(a_string_including('application/json'))
+    #   end
+    # end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'destroys the requested booking' do
+      booking = create(:booking, professional_id: professional.id, client_id: client.id)
+      expect do
+        delete booking_url(booking), headers: valid_headers, as: :json
+      end.to change(Booking, :count).by(-1)
+    end
+  end
 end
