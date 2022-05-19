ae3ae7bf8276194049dc0e6cec13fb7a7626af8b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-19 13:14:58 +0800

    Removed comment after create review. Add trait admin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -21,12 +21,6 @@ FactoryBot.define do
       role { 'client' }
 
       # after(:create) do |user|
-      #   review = Review.new(attributes_for(:review))
-      #   review.client_id = user.client.id
-      #   review.save
-      # end
-
-      # after(:create) do |user|
       #   connection = Connection.new(
       #     professional_id: 1,
       #     client_id: user.client.id,
@@ -41,6 +35,13 @@ FactoryBot.define do
       contact_number { '9158765432' }
       email { 'test@email.com' }
       role { 'professional' }
+    end
+
+    trait :admin do
+      id { 4 }
+      contact_number { '9134652874' }
+      email { 'admin@email.com' }
+      role { 'admin' }
     end
   end
 end
