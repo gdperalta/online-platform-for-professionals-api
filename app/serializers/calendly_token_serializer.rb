1f75869b6ea9cc7efd940d890c5fed9314276b45
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-21 12:12:12 +0800

    Add scheduling_url attribute to calendly_token and assign value of response scheduling when calling Calendly::Client.user

--- app/serializers/calendly_token_serializer.rb
+++ app/serializers/calendly_token_serializer.rb
@@ -1,6 +1,6 @@
 class CalendlyTokenSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
-  attributes :authorization, :user_uri, :organization_uri
+  attributes :authorization, :user_uri, :organization_uri, :scheduling_url
   belongs_to :professional
 end
