899acb41d2a0fa53c1fdf47f32516f4160c7c6eb
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-05-23 16:15:55 +0800

    Add pending to in set_parameters case when In handle_events, add condition to next each loop depending on status Add event_uuid method to get event id in link add event_exists method to check presence of booking

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -47,7 +47,7 @@ class Professional < ApplicationRecord
     when 'active'
       parameters[:status] = 'active'
       parameters[:min_start_time] = Time.now
-    when 'finished'
+    when 'pending', 'finished'
       parameters[:status] = 'active'
       parameters[:max_start_time] = Time.now
     when 'canceled'
@@ -64,21 +64,33 @@ class Professional < ApplicationRecord
     return if events_list.empty?
 
     events_list.each do |event|
+      event_exists = event_exists?(event)
+
+      next if @status == 'pending' && event_exists.present?
+      next if @status == 'finished' && event_exists.nil?
+
       @events << event_data(event)
     end
   end
 
   def event_data(event)
-    event_uuid = event['uri'].split('events/').last
-    response = Calendly::Client.event_invitee(calendly_token.authorization, event_uuid)
+    response = Calendly::Client.event_invitee(calendly_token.authorization, event_uuid(event))
     event['invitee_uri'] = response['uri']
     event['invitee_email'] = response['email']
     event['invite_name'] = response['name']
 
     {
-      id: event_uuid,
+      id: event_uuid(event),
       type: "#{@status}_events",
       attributes: event.except('created_at', 'event_guests', 'event_memberships', 'updated_at')
     }
+  end
+
+  def event_exists?(event)
+    Booking.find_by(event_uuid: event_uuid(event))
+  end
+
+  def event_uuid(event)
+    event['uri'].split('events/').last
   end
 end
